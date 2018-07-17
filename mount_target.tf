cbb44d583d8cd41a82e06cbab1f88e18fb0bb5f1
Christopher Edsall <chris.edsall@bristol.ac.uk>
2018-07-17 14:17:18 +0100

    Loop over correct array for mount targets
    We want to loop over var.ADS

--- mount_target.tf
+++ mount_target.tf
@@ -3,5 +3,5 @@ resource "oci_file_storage_mount_target" "ClusterFSMountTarget" {
   count               = "${length(var.ADS)}"
   availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.ADS[count.index] -1 ], "name")}"
   compartment_id      = "${var.compartment_ocid}"
-  subnet_id           = "${oci_core_subnet.ClusterSubnet.*.id[index(var.ADS, var.InstanceADIndex[count.index])]}"
+  subnet_id           = "${oci_core_subnet.ClusterSubnet.*.id[index(var.ADS, var.ADS[count.index]) ]}"
 }
