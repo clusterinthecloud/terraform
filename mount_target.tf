1bc80e8be0fd642a3553423418133ff216203c2d
Matt Williams <matt@milliams.com>
2018-12-03 11:52:29 +0000

    Only create one mount point for the whole cluster

--- mount_target.tf
+++ mount_target.tf
@@ -1,8 +1,7 @@
 resource "oci_file_storage_mount_target" "ClusterFSMountTarget" {
-  count               = "${length(var.ADS)}"
-  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.ADS[count.index] -1 ], "name")}"
+  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.FilesystemAD - 1], "name")}"
   compartment_id      = "${var.compartment_ocid}"
-  subnet_id           = "${oci_core_subnet.ClusterSubnet.*.id[index(var.ADS, var.ADS[count.index]) ]}"
-  display_name        = "fileserverAD${var.ADS[count.index]}"
-  hostname_label      = "fileserverAD${var.ADS[count.index]}"
+  subnet_id           = "${oci_core_subnet.ClusterSubnet.*.id[index(var.ADS, var.FilesystemAD) ]}"
+  display_name        = "fileserver"
+  hostname_label      = "fileserver"
 }
