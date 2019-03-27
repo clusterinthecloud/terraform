1f617de2cf1458a54eaa9d3c6ead04db984c64f4
Matt Williams <matt@milliams.com>
2019-03-27 13:18:29 +0000

    Only kill nodes if Slurm is running

--- compute.tf
+++ compute.tf
@@ -99,9 +99,11 @@ EOF
     when = "destroy"
     inline = [
       "echo Terminating any remaining compute nodes",
+      "if systemctl status slurmctld >> /dev/null; then",
       "sudo -u slurm /usr/local/bin/stopnode \"$(sinfo --noheader --Format=nodelist:10000 | tr -d '[:space:]')\" || true",
+      "fi",
       "sleep 5",
-      "echo Node termination requested",
+      "echo Node termination request completed",
     ]
 
     connection {
