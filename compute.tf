d8a5bf6fd5580f24482ff9b32d1b5cbb482cd7df
Matt Williams <matt@milliams.com>
2019-04-01 16:45:08 +0100

    Add ansible_branch to config file

--- compute.tf
+++ compute.tf
@@ -102,6 +102,7 @@ region: ${var.region}
 compartment_id: ${var.compartment_ocid}
 vcn_id: ${oci_core_virtual_network.ClusterVCN.id}
 ad_root: ${substr(oci_core_instance.ClusterManagement.availability_domain, 0, length(oci_core_instance.ClusterManagement.availability_domain)-1)}
+ansible_branch: ${var.ansible_branch}
 EOF
 
     connection {
