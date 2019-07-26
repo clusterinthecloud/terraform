229fcc060f68bbff562916ac5863eabf1e6f2845
Matt Williams <matt@milliams.com>
2019-07-26 16:39:12 +0100

    Fix version to 11 for now

--- oracle-cloud-infrastructure/provider.tf
+++ oracle-cloud-infrastructure/provider.tf
@@ -1,3 +1,7 @@
+terraform             {required_version = "~> 0.11.13"}
+provider "template"   {version = "~> 2.1"}
+provider "tls"        {version = "~> 2.0"}
+
 provider "oci" {
   version          = ">= 3.23.0"
 
