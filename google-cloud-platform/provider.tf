b3a4199fcdc0eaf3e186a2e989d290864feb0c29
Matt Williams <matt@milliams.com>
2019-08-02 11:15:47 +0100

    Fix syntax so that the credentials variable is just the path

--- google-cloud-platform/provider.tf
+++ google-cloud-platform/provider.tf
@@ -7,7 +7,7 @@ provider "local"      {version = "1.3"}
 
 provider "google" {
   version = "2.10"
-  credentials = "${var.credentials}"
+  credentials = "${file(var.credentials)}"
   region  = "${var.gcp_region}"
   zone    = "${var.gcp_zone}"
   project = "${var.gcp_project}"
