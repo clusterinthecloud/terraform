439415f1dab157c970ec43f79708a9103b04daec
Matt Williams <matt@milliams.com>
2019-11-15 14:24:47 +0000

    Add info to the startnode config file

--- aws/data-sources.tf
+++ aws/data-sources.tf
@@ -11,6 +11,11 @@ data "template_file" "startnode-yaml" {
   template = file("${path.module}/files/startnode.yaml.tpl")
   vars = {
     ansible_branch = var.ansible_branch
+    region = var.region
+    subnet = aws_subnet.vpc_subnetwork.id
+    compute_security_group = aws_security_group.mgmt.id
+    dns_zone = aws_route53_zone.cluster.name
+    dns_zone_id = aws_route53_zone.cluster.zone_id
   }
 }
 
--- aws/files/startnode.yaml.tpl
+++ aws/files/startnode.yaml.tpl
@@ -1,1 +1,6 @@
 ansible_branch: ${ansible_branch}
+region: ${region}
+subnet: ${subnet}
+compute_security_group: ${compute_security_group}
+dns_zone: ${dns_zone}
+dns_zone_id: ${dns_zone_id}
