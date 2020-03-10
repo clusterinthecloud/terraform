ce5cccbd3bbfe97ef46b12019d0e2c8b4783bdf5
Matt Williams <matt@milliams.com>
2020-03-10 13:16:10 +0000

    Open HTTP port

--- aws/security-groups.tf
+++ aws/security-groups.tf
@@ -11,6 +11,13 @@ resource "aws_security_group" "mgmt" {
   }
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
