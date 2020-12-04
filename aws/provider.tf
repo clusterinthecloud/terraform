06496a0bb4d80f66cbb54117083b93010e912236
Matt Williams <matt@milliams.com>
2020-12-04 14:35:55 +0000

    Use the new provider requirements
    https://www.terraform.io/docs/configuration/provider-requirements.html

--- aws/provider.tf
+++ aws/provider.tf
@@ -1,29 +1,34 @@
 terraform {
-  required_version = "~> 0.12"
-}
-
-provider "template" {
-  version = "2.1"
-}
-
-provider "external" {
-  version = "1.2"
-}
-
-provider "local" {
-  version = "1.3"
-}
-
-provider "random" {
-  version = "~> 2.2"
-}
-
-provider "null" {
-  version = "~> 2.1"
+  required_version = "~> 0.13"
+  required_providers {
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.1"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 1.2"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 1.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 2.2"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 2.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.16.0"
+    }
+  }
 }
 
 provider "aws" {
-  version     = "2.16.0"
   profile     = var.profile  # refer to ~/.aws/credentials
   region      = var.region
 }
