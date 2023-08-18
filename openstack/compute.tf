a1eb2bf82851409fa9f85fb6eb38a6c076b9d70c
Matt Williams <matt@milliams.com>
2023-08-18 17:32:05 +0100

    Add the security rules

--- openstack/compute.tf
+++ openstack/compute.tf
@@ -8,12 +8,12 @@ locals {
 }
 
 resource "openstack_compute_keypair_v2" "citc_admin" {
-  name       = "citc-keypair-${local.cluster_id}"
+  name       = "citc-admin-${local.cluster_id}"
   public_key = var.ssh_public_key
 }
 
-resource "openstack_networking_secgroup_v2" "mgmt" {
-  name = "secgroup-${local.cluster_id}"
+resource "openstack_networking_secgroup_v2" "external" {
+  name = "external-${local.cluster_id}"
   description = "Access to the mgmt node of ${local.cluster_id}"
 }
 
@@ -24,13 +24,28 @@ resource "openstack_networking_secgroup_rule_v2" "external_ssh_all" {
   port_range_min    = 22
   port_range_max    = 22
   remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.mgmt.id
+  security_group_id = openstack_networking_secgroup_v2.external.id
+}
+
+resource "openstack_networking_secgroup_v2" "cluster" {
+  name = "cluster-${local.cluster_id}"
+  description = "Access to the mgmt node of ${local.cluster_id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "cluster_all_ipv4" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  remote_ip_prefix  = openstack_networking_subnet_v2.cluster.cidr
+  security_group_id = openstack_networking_secgroup_v2.cluster.id
 }
 
 resource "openstack_compute_instance_v2" "mgmt" {
   name = local.mgmt_hostname
   flavor_name = var.mgmt_flavor
-  security_groups = [openstack_networking_secgroup_v2.mgmt.name]
+  security_groups = [
+    openstack_networking_secgroup_v2.external.name,
+    openstack_networking_secgroup_v2.cluster.name,
+  ]
   key_pair = openstack_compute_keypair_v2.citc_admin.name
 
   user_data = base64encode(data.template_file.user_data.rendered)
